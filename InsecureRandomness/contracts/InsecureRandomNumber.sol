b25d2c4d5978d860f0c78b89cc296681ca8f22bc
TamarawGuy <viktor.m.rumenov@gmail.com>
2023-07-22 16:11:50 +0300

    Add guess random number contract for hacking

---
+++ InsecureRandomness/contracts/InsecureRandomNumber.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.18;
+
+contract GuessTheNumber {
+    constructor() payable {}
+
+    function guess(uint _guess) public {
+        uint answer = uint(
+            keccak256(
+                abi.encodePacked(blockhash(block.number - 1), block.timestamp)
+            )
+        );
+
+        if (_guess == answer) {
+            (bool sent, ) = msg.sender.call{value: 1 ether}("");
+            require(sent, "Failed to send Ether");
+        }
+    }
+}
